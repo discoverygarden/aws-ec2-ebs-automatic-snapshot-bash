5fe9513c282163b71b01a8b59b20a5fc672f334c
Gervais <gervais@demontbrun.com>
2018-10-02 09:45:26 -0300

    ws
    Fixed inconsisten use with tabs vs spaces and number of spaces for
    indentation

--- ebs-snapshot.sh
+++ ebs-snapshot.sh
@@ -42,78 +42,78 @@ retention_date_in_seconds=$(date +%s --date "$retention_days days ago")
 
 # Function: Setup logfile and redirect stdout/stderr.
 log_setup() {
-    # Check if logfile exists and is writable.
-    ( [ -e "$logfile" ] || touch "$logfile" ) && [ ! -w "$logfile" ] && echo "ERROR: Cannot write to $logfile. Check permissions or sudo access." && exit 1
+  # Check if logfile exists and is writable.
+  ( [ -e "$logfile" ] || touch "$logfile" ) && [ ! -w "$logfile" ] && echo "ERROR: Cannot write to $logfile. Check permissions or sudo access." && exit 1
 
-    tmplog=$(tail -n $logfile_max_lines $logfile 2>/dev/null) && echo "${tmplog}" > $logfile
-    exec > >(tee -a $logfile)
-    exec 2>&1
+  tmplog=$(tail -n $logfile_max_lines $logfile 2>/dev/null) && echo "${tmplog}" > $logfile
+  exec > >(tee -a $logfile)
+  exec 2>&1
 }
 
 # Function: Log an event.
 log() {
-    echo "[$(date +"%Y-%m-%d"+"%T")]: $*"
+  echo "[$(date +"%Y-%m-%d"+"%T")]: $*"
 }
 
 # Function: Confirm that the AWS CLI and related tools are installed.
 prerequisite_check() {
-	for prerequisite in aws wget; do
-		hash $prerequisite &> /dev/null
-		if [[ $? == 1 ]]; then
-			echo "In order to use this script, the executable \"$prerequisite\" must be installed." 1>&2; exit 70
-		fi
-	done
+  for prerequisite in aws wget; do
+    hash $prerequisite &> /dev/null
+    if [[ $? == 1 ]]; then
+      echo "In order to use this script, the executable \"$prerequisite\" must be installed." 1>&2; exit 70
+    fi
+  done
 }
 
 # Function: Check for volumes to exclude from snapshots
 check_for_exclusions() {
-	exclusions=$*
-	if [ -n "$exclusions" ]; then
-		for exclusion in $exclusions; do
-			volume_list=$(echo $volume_list | sed -e "s/$exclusion//")
-		done
-	fi
+  exclusions=$*
+  if [ -n "$exclusions" ]; then
+    for exclusion in $exclusions; do
+      volume_list=$(echo $volume_list | sed -e "s/$exclusion//")
+    done
+  fi
 }
 
 # Function: Snapshot all volumes attached to this instance.
 snapshot_volumes() {
-	for volume_id in $volume_list; do
-		log "Volume ID is $volume_id"
+  for volume_id in $volume_list; do
+    log "Volume ID is $volume_id"
 
-		# Get the attched device name to add to the description so we can easily tell which volume this is.
-		device_name=$(aws ec2 describe-volumes --region $region --output=text --volume-ids $volume_id --query 'Volumes[0].{Devices:Attachments[0].Device}')
+    # Get the attched device name to add to the description so we can easily tell which volume this is.
+    device_name=$(aws ec2 describe-volumes --region $region --output=text --volume-ids $volume_id --query 'Volumes[0].{Devices:Attachments[0].Device}')
 
-		# Take a snapshot of the current volume, and capture the resulting snapshot ID
-		snapshot_description="$(hostname)-$device_name-backup-$(date +%Y-%m-%d)"
+    # Take a snapshot of the current volume, and capture the resulting snapshot ID
+    snapshot_description="$(hostname)-$device_name-backup-$(date +%Y-%m-%d)"
 
-		snapshot_id=$(aws ec2 create-snapshot --region $region --output=text --description $snapshot_description --volume-id $volume_id --query SnapshotId)
-		log "New snapshot is $snapshot_id"
+    snapshot_id=$(aws ec2 create-snapshot --region $region --output=text --description $snapshot_description --volume-id $volume_id --query SnapshotId)
+    log "New snapshot is $snapshot_id"
 
-		# Add a "CreatedBy:AutomatedBackup" tag to the resulting snapshot.
-		# Why? Because we only want to purge snapshots taken by the script later, and not delete snapshots manually taken.
-		aws ec2 create-tags --region $region --resource $snapshot_id --tags Key=CreatedBy,Value=AutomatedBackup
-	done
+    # Add a "CreatedBy:AutomatedBackup" tag to the resulting snapshot.
+    # Why? Because we only want to purge snapshots taken by the script later, and not delete snapshots manually taken.
+    aws ec2 create-tags --region $region --resource $snapshot_id --tags Key=CreatedBy,Value=AutomatedBackup
+  done
 }
 
 # Function: Cleanup all snapshots associated with this instance that are older than $retention_days
 cleanup_snapshots() {
-	for volume_id in $volume_list; do
-		snapshot_list=$(aws ec2 describe-snapshots --region $region --output=text --filters "Name=volume-id,Values=$volume_id" "Name=tag:CreatedBy,Values=AutomatedBackup" --query Snapshots[].SnapshotId)
-		for snapshot in $snapshot_list; do
-			log "Checking $snapshot..."
-			# Check age of snapshot
-			snapshot_date=$(aws ec2 describe-snapshots --region $region --output=text --snapshot-ids $snapshot --query Snapshots[].StartTime | awk -F "T" '{printf "%s\n", $1}')
-			snapshot_date_in_seconds=$(date "--date=$snapshot_date" +%s)
-			snapshot_description=$(aws ec2 describe-snapshots --snapshot-id $snapshot --region $region --query Snapshots[].Description)
+  for volume_id in $volume_list; do
+    snapshot_list=$(aws ec2 describe-snapshots --region $region --output=text --filters "Name=volume-id,Values=$volume_id" "Name=tag:CreatedBy,Values=AutomatedBackup" --query Snapshots[].SnapshotId)
+    for snapshot in $snapshot_list; do
+      log "Checking $snapshot..."
+      # Check age of snapshot
+      snapshot_date=$(aws ec2 describe-snapshots --region $region --output=text --snapshot-ids $snapshot --query Snapshots[].StartTime | awk -F "T" '{printf "%s\n", $1}')
+      snapshot_date_in_seconds=$(date "--date=$snapshot_date" +%s)
+      snapshot_description=$(aws ec2 describe-snapshots --snapshot-id $snapshot --region $region --query Snapshots[].Description)
 
-			if (( $snapshot_date_in_seconds <= $retention_date_in_seconds )); then
-				log "DELETING snapshot $snapshot. Description: $snapshot_description ..."
-				aws ec2 delete-snapshot --region $region --snapshot-id $snapshot
-			else
-				log "Not deleting snapshot $snapshot. Description: $snapshot_description ..."
-			fi
-		done
-	done
+      if (( $snapshot_date_in_seconds <= $retention_date_in_seconds )); then
+        log "DELETING snapshot $snapshot. Description: $snapshot_description ..."
+        aws ec2 delete-snapshot --region $region --snapshot-id $snapshot
+      else
+        log "Not deleting snapshot $snapshot. Description: $snapshot_description ..."
+      fi
+    done
+  done
 }
 
 
